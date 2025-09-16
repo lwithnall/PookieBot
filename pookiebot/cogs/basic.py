4b666867199bd95edb2ada71a461e0eadffe0e78
Liam <liam@kayosdesign.com>
2025-09-16 12:03:10 +1000

    Added documentation for planned functionality; Began boilerplate bot set-up

---
+++ pookiebot/cogs/basic.py
@@ -0,0 +1,6 @@
+"""
+Planned basic methods:
+> echo: repeat user message
+> coin flip: return heads or tails
+> choose: make choice from provided options
+"""
---
+++ pookiebot/cogs/currency_conversion.py
@@ -0,0 +1,6 @@
+"""
+Basic currency conversion command
+Get conversion values from x-rates.com (unless better method found)
+"""
+
+XRATES_LINK = "https://www.x-rates.com/table/?from=AUD&amount="
